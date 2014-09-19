109c940d5fb3c8b0e1740c87d1be864389a7739d
Mukesh Yadav <mukeshyadav@gmail.com>
2014-09-19 18:00:09 +0530

    Update jquery.my.wrap.js

--- jquery.my.wrap.js
+++ jquery.my.wrap.js
@@ -1,119 +1,64 @@
-;(function( $, window, document, undefined) {
-	"use strict";
+/*
+	* jQuery plugin for content show more/less
+	* Author: yatra.com team (http://www.yatra.com)
+	* Further changes, comments, commit: http://www.github.com/mukeshyadav
+	* Licensed under the MIT license
+*/
 
-	var pluginName = "myWrap",
-		config = {
-			wrapperName: 		'my-wrap',
-			innerWrapElem: 		'p',
-			wrapLength: 		100,
-			wrapElipsis: 		true,
-			wrapClick: 			'my-click',
-			textHideClass: 		'hide',
-			textVisibleClass: 	'show',
-			textElpsClass: 		'ico-elps',
-			showText: 			'More',
-			hideText: 			'Less',
-			icoMore: 			'ico-more',
-			icoLess: 			'ico-less'
+;(function( $, window, document, undefined ){
+	"use strict";
+	var pluginName = "myToggle", 
+		defaults = {
+			showItem: 4,
+			elemBlock: 'ul',
+			elemTag: 'li',
+			textHide: 'Hide More',
+			textShow: 'Show More'
 		};
 
-	function MyWrap( element, options ) {
+	function MyToggle( element, options ){
 		this.element = element;
-		this.options = $.extend( {}, config, options );
-
-		this._config = config;
+		this.options = $.extend( {}, defaults, options ), 
+		this._defaults = defaults,
 		this._name = pluginName;
 
-		this.setWrap();
-	}
+		this.init();
+	};
 
-	MyWrap.prototype.setWrap = function() {
-		var opt = this.options,
-			wrapElem = $(this.element),
-			innerWrapElem =wrapElem.find(opt.innerWrapElem),
-			textLength = wrapElem.data('limit') || opt.wrapLength,
-			isElips = opt.wrapElipsis;
-
-		this.geTextLength(innerWrapElem) > textLength ? 
-					 this.createWrap(wrapElem, innerWrapElem, textLength, isElips, this.geTextLength(innerWrapElem)) : false;
-	}
-
-	MyWrap.prototype.geTextLength = function(inElem) {
-		return inElem.text().length;
-	}
-
-	MyWrap.prototype.createWrap = function(el, inEl, wLen, wElps, elLen) {
-		var opt 		= this.options,
-			inElm 		= inEl.text(),
-			elmLen 		= elLen,
-			wrapLen 	= wLen,
-			strShow 	= inElm.substr(0, wrapLen),
-			strHide 	= inElm.substr(wrapLen, elmLen), 
-			strElps 	= ( wElps == true ? "<span class='"+ opt.textElpsClass +"'>&hellip;</span>" : ""),
-			newStr 		= strShow 
-					 		+ "<span class='"+ opt.textHideClass +"'>" + strHide + "</span>" 
-					 		+ strElps; 
-
-			inEl.text("").append(newStr);
-
-			this.clickMore(el);
-	}
-
-	MyWrap.prototype.clickMore = function(elem) {
-		var opt = this.options,
-			_this = this,
-			el = elem;
-
-		$(elem).on('click', '.' + opt.wrapClick, function(e){
+	MyToggle.prototype.attachEvents = function() {
+		var that = this,
+			elem = $(this.element);
+		elem.on('click', function(e){
 			e.preventDefault();
-			var type = $(this).data('type');
-			
-			type === 'more' ? _this.showMore(this, el) : _this.showLess(this, el);
-		});
-	}
-
-	MyWrap.prototype.showMore = function(elThis, elem){
-		var opt = this.options,
-			hide = opt.textHideClass,
-			show = opt.textVisibleClass,
-			el = elem,
-			elm = $(elThis),
-			elmText = elm.text();
-
-		el.find('.' + hide).addClass( show ).removeClass(hide);
-		elm.data('type', 'less');
-		elm.html( this.textReplace( elm.html(), opt.showText, opt.hideText ));
-		elm.find('.' + opt.icoMore).removeClass(opt.icoMore).addClass(opt.icoLess);
-		el.find('.' + opt.textElpsClass).addClass(opt.textHideClass);
-	}
-
-	MyWrap.prototype.showLess = function(elThis, elem) {
-		var opt = this.options,
-			hide = opt.textHideClass,
-			show = opt.textVisibleClass,
-			el = elem,
-			elm = $(elThis);
-			console.log(elm)
-
-		el.find('.' + show).addClass( hide ).addClass(show);
-		elm.data('type', 'more');
-		elm.html( this.textReplace( elm.html(), opt.hideText, opt.showText ));
-		elm.find('.' + opt.icoLess).removeClass(opt.icoLess).addClass(opt.icoMore);
-		el.find('.' + opt.textElpsClass).removeClass(opt.textHideClass);
-	}
-
-	MyWrap.prototype.textReplace = function(actStr, acTxt, repTxt){
-		var str = actStr.replace(acTxt, repTxt);
-		return str;
-	}
-
-	$.fn[pluginName] = function( options ) {
-		return this.each( function(){
-			if(!$.data(this, 'plugin_' + pluginName )) {
-				$.data(this, 'plugin_' + pluginName, 
-				new MyWrap( this, options ));
+			if( $(this).hasClass('ico-less') ) {
+				elem.text(that.options.textShow).addClass('ico-more')
+					.removeClass('ico-less').parent(that.options.elemBlock).find(that.options.elemTag + ':gt('+ that.options.showItem +')').slideUp();
+			} else { 
+				elem.text(that.options.textHide).addClass('ico-less')
+					.removeClass('ico-more').parent(that.options.elemBlock).find(that.options.elemTag).not(':visible').slideDown();
 			}
 		})
+	};
+
+	MyToggle.prototype.init = function() {
+		this.setToggle();
+		this.attachEvents();
 	}
 
-})( jQuery, window, document );
+	MyToggle.prototype.setToggle = function() {
+		var that = this,
+			option = that.options,
+			elem = $(this.element);	
+			
+			elem.parent(option.elemBlock).find(option.elemTag + ':gt('+ option.showItem +')').hide();
+	}
+
+	$.fn[pluginName] = function( options ){
+		return this.each( function() {
+			if( !$.data(this, 'plugin_' + pluginName )) {
+				$.data(this, 'plugin_' + pluginName, new MyToggle( this, options ));
+			}
+		});
+	};
+
+})(jQuery);
